5b55c7bd8ab157c5d6d141f318b5427e69ff5001
Admin <Admin@ADMIN->
2018-10-07 02:31:00 +0500

    Добавьте файлы проекта.

---
+++ task2.3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace task2._3
+{
+    class Program
+    {
+        static int Number(string txt)
+        {
+            txt = txt.Trim(' ');
+            int num = Convert.ToInt32(txt);
+            return num;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            FileStream f = new FileStream("input.txt", FileMode.OpenOrCreate);
+            StreamReader r = new StreamReader(f);
+            string s1 = r.ReadLine();
+            int[] arr = new int[Convert.ToInt32(s1)];
+            arr[arr.Length - 1] = Convert.ToInt32(s1);
+            int a = 1;
+            for (int i =0;i<arr.Length;i++)
+            {
+                arr[i] = a;
+                a++;
+            }
+            r.Close();
+            f.Close();
+            FileStream f1 = new FileStream("output.txt", FileMode.OpenOrCreate);
+            StreamWriter w = new StreamWriter(f1);
+            int tmp = arr[arr.Length - 1];
+            if (arr.Length%2 != 0)
+            {
+                for (int i = arr.Length - 1; i > arr.Length / 2; i--)
+                {
+                    arr[i] = arr[i - 1];
+                }
+                arr[arr.Length / 2] = tmp;
+            }
+            else
+            {
+                for (int i = arr.Length - 1; i > (arr.Length / 2-1); i--)
+                {
+                    arr[i] = arr[i - 1];
+                }
+                arr[arr.Length / 2-1] = tmp;
+            }
+            foreach (int x in arr)
+            {
+                w.Write(x + " ");
+            }
+            w.Close();
+            f1.Close();
+        }
+    }
+}
---
+++ task2.3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("task2.3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task2.3")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5158e601-6739-42b5-a8a1-59230f3b88d7")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
