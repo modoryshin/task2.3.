207db1bf024537934043f382fee6e6f1c70bba43
Admin <Admin@ADMIN->
2018-10-07 03:24:00 +0500

    yaaaz

--- task2.3/Program.cs
+++ task2.3/Program.cs
@@ -41,13 +41,21 @@ namespace task2._3
                 arr[2] = arr[1];
                 arr[1] = tmp;
             }
-            if (arr.Length != 2&&arr.Length!=3&&arr.Length!=1)
+            if (arr.Length%2==0&&arr.Length != 2&&arr.Length!=3&&arr.Length!=1)
             {
                 for (int i = arr.Length - 1; i > arr.Length / 2-1; i--)
                 {
                     arr[i] = arr[i - 1];
                 }
                 arr[arr.Length / 2-1] = tmp;
+            }
+            if (arr.Length%2!=0&&arr.Length != 2 && arr.Length != 3 && arr.Length != 1)
+            {
+                for (int i = arr.Length-1 ; i > arr.Length / 2 ; i--)
+                {
+                    arr[i] = arr[i - 1];
+                }
+                arr[arr.Length / 2 ] = tmp;
             }
             foreach (int x in arr)
             {
