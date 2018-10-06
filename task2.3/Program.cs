5b2fb9786a4e8833b9229212d7244c2bd9d07faf
Admin <Admin@ADMIN->
2018-10-07 02:42:00 +0500

    вот

--- task2.3/Program.cs
+++ task2.3/Program.cs
@@ -36,11 +36,22 @@ namespace task2._3
             FileStream f1 = new FileStream("output.txt", FileMode.OpenOrCreate);
             StreamWriter w = new StreamWriter(f1);
             int tmp = arr[arr.Length - 1];
+            if (arr.Length % 2 != 0&& arr.Length != 2)
+            {
                 for (int i = arr.Length - 1; i > arr.Length / 2; i--)
                 {
                     arr[i] = arr[i - 1];
                 }
                 arr[arr.Length / 2] = tmp;
+            }
+            if (arr.Length % 2 == 0 && arr.Length != 2)
+            {
+                for (int i = arr.Length - 1; i > arr.Length / 2-1; i--)
+                {
+                    arr[i] = arr[i - 1];
+                }
+                arr[arr.Length / 2-1] = tmp;
+            }
             foreach (int x in arr)
             {
                 w.Write(x + " ");
